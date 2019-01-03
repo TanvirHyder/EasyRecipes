699303e0564cb42b82102a019a3fc9dedb90ed55
Oliver Phan <oliver.phan@mail.utoronto.ca>
2019-01-02 22:21:39 -0500

    Updated Navigation package and variable naming

--- AppScreens/MainMenu.js
+++ AppScreens/MainMenu.js
@@ -15,8 +15,9 @@ export default class MainMenu extends React.Component {
 
     render() {
 
-        let Ingredients = this.state.ingredientArray.map( (val,key) => {
-            return <Ingredient key={key} keyval={key} val={val}
+        let Ingredients = this.state.ingredientArray.map( (val,index) => {
+            // These are props, key, keyval, val, deleteMethod
+            return <Ingredient key={index} keyval={index} val={val}
             deleteMethod = { () => this.deleteIngredient(key)} />
         });
 
@@ -160,7 +161,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
 
-    mainBox: {
+    ingredientsBox: {
         width: 275,
         justifyContent: 'center',
         backgroundColor: '#E0E0E0', // E0E0E0
